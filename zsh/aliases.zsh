031d5dfe82f7e51b18c0d1ae6ea288c875cdb691
Jeff Billimek <jeffrey_k_billimek@homedepot.com>
2016-05-05 17:34:48 -0400

    commenting-out a lot of unwanted aliases (for now)

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,11 +13,11 @@ elif [[ $unamestr == 'Darwin' ]]; then
 fi
 
 # YADR support
-alias yav='yadr vim-add-plugin'
-alias ydv='yadr vim-delete-plugin'
-alias ylv='yadr vim-list-plugin'
-alias yup='yadr update-plugins'
-alias yip='yadr init-plugins'
+#alias yav='yadr vim-add-plugin'
+#alias ydv='yadr vim-delete-plugin'
+#alias ylv='yadr vim-list-plugin'
+#alias yup='yadr update-plugins'
+#alias yip='yadr init-plugins'
 
 # PS
 alias psa="ps aux"
@@ -43,7 +43,7 @@ elif [[ $platform == 'darwin' ]]; then
 fi
 
 # show me files matching "ls grep"
-alias lsg='ll | grep'
+#alias lsg='ll | grep'
 
 # Alias Editing
 TRAPHUP() {
@@ -65,10 +65,10 @@ fi
 alias :q='exit'
 
 # vimrc editing
-alias ve='vim ~/.vimrc'
+#alias ve='vim ~/.vimrc'
 
 # zsh profile editing
-alias ze='vim ~/.zshrc'
+#alias ze='vim ~/.zshrc'
 
 # Git Aliases
 alias gs='git status'
@@ -136,7 +136,7 @@ alias gdmb='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 
 # Common shell functions
 alias less='less -r'
-alias tf='tail -f'
+alias tf='tail -F'
 alias l='less'
 alias lh='ls -alt | head' # see the last modified files
 alias screen='TERM=screen screen'
@@ -146,19 +146,19 @@ alias cl='clear'
 alias gz='tar -zcvf'
 
 # Ruby
-alias c='rails c' # Rails 3
-alias co='script/console' # Rails 2
-alias cod='script/console --debugger'
+#alias c='rails c' # Rails 3
+#alias co='script/console' # Rails 2
+#alias cod='script/console --debugger'
 
 #If you want your thin to listen on a port for local VM development
 #export VM_IP=10.0.0.1 <-- your vm ip
-alias ts='thin start -a ${VM_IP:-127.0.0.1}'
-alias ms='mongrel_rails start'
-alias tfdl='tail -f log/development.log'
-alias tftl='tail -f log/test.log'
+#alias ts='thin start -a ${VM_IP:-127.0.0.1}'
+#alias ms='mongrel_rails start'
+#alias tfdl='tail -f log/development.log'
+#alias tftl='tail -f log/test.log'
 
-alias ka9='killall -9'
-alias k9='kill -9'
+#alias ka9='killall -9'
+#alias k9='kill -9'
 
 # Gem install
 alias sgi='sudo gem install --no-ri --no-rdoc'
@@ -166,31 +166,31 @@ alias sgi='sudo gem install --no-ri --no-rdoc'
 # TODOS
 # This uses NValt (NotationalVelocity alt fork) - http://brettterpstra.com/project/nvalt/
 # to find the note called 'todo'
-alias todo='open nvalt://find/todo'
+#alias todo='open nvalt://find/todo'
 
 # Forward port 80 to 3000
-alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
+#alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
 
-alias rdm='rake db:migrate'
-alias rdmr='rake db:migrate:redo'
+#alias rdm='rake db:migrate'
+#alias rdmr='rake db:migrate:redo'
 
 # Zeus
-alias zs='zeus server'
-alias zc='zeus console'
-alias zr='zeus rspec'
-alias zrc='zeus rails c'
-alias zrs='zeus rails s'
-alias zrdbm='zeus rake db:migrate'
-alias zrdbtp='zeus rake db:test:prepare'
+#alias zs='zeus server'
+#alias zc='zeus console'
+#alias zr='zeus rspec'
+#alias zrc='zeus rails c'
+#alias zrs='zeus rails s'
+#alias zrdbm='zeus rake db:migrate'
+#alias zrdbtp='zeus rake db:test:prepare'
 
 # Rspec
-alias rs='rspec spec'
-alias sr='spring rspec'
-alias src='spring rails c'
-alias srgm='spring rails g migration'
-alias srdm='spring rake db:migrate'
-alias srdt='spring rake db:migrate'
-alias srdmt='spring rake db:migrate db:test:prepare'
+#alias rs='rspec spec'
+#alias sr='spring rspec'
+#alias src='spring rails c'
+#alias srgm='spring rails g migration'
+#alias srdm='spring rake db:migrate'
+#alias srdt='spring rake db:migrate'
+#alias srdmt='spring rake db:migrate db:test:prepare'
 
 
 # Sprintly - https://github.com/nextbigsoundinc/Sprintly-GitHub
@@ -205,11 +205,11 @@ alias grb='git recent-branches'
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 
-alias dbtp='spring rake db:test:prepare'
-alias dbm='spring rake db:migrate'
-alias dbmr='spring rake db:migrate:redo'
-alias dbmd='spring rake db:migrate:down'
-alias dbmu='spring rake db:migrate:up'
+#alias dbtp='spring rake db:test:prepare'
+#alias dbm='spring rake db:migrate'
+#alias dbmr='spring rake db:migrate:redo'
+#alias dbmd='spring rake db:migrate:down'
+#alias dbmu='spring rake db:migrate:up'
 
 # Homebrew
-alias brewu='brew update  && brew upgrade --all && brew cleanup && brew prune && brew doctor'
+alias brewu='brew update && brew upgrade --all && brew cleanup && brew prune && brew doctor'
