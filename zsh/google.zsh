d123f18bc52ffdb5887d118304fa26e48d9d63a6
sqdev <associate@sqdev-iMac-28f07620832a-SQDEV.ssc.homedepot.com>
2016-05-05 16:36:44 -0400

    split-out zshrc stuff to seperate files

---
+++ zsh/google.zsh
@@ -0,0 +1,10 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [[ -s "/Users/jeff/google-cloud-sdk/path.zsh.inc" ]]; then
+   source '/Users/jeff/google-cloud-sdk/path.zsh.inc'
+fi
+
+# The next line enables shell command completion for gcloud.
+if [[ -s "/Users/jeff/google-cloud-sdk/completion.zsh.inc" ]]; then
+   source '/Users/jeff/google-cloud-sdk/completion.zsh.inc'
+fi
+
