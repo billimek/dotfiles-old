437201876b3be8bd2e24f5a207a93f7ad200f084
Jeff Billimek <jeffrey_k_billimek@homedepot.com>
2016-05-06 10:18:25 -0400

    switching to brew cask version of gcloud stuff

--- zsh/google.zsh
+++ zsh/google.zsh
@@ -1,10 +1,9 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [[ -s "/Users/jeff/google-cloud-sdk/path.zsh.inc" ]]; then
-   source '/Users/jeff/google-cloud-sdk/path.zsh.inc'
+if [[ -s '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc' ]]; then
+  source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
 fi
 
 # The next line enables shell command completion for gcloud.
-if [[ -s "/Users/jeff/google-cloud-sdk/completion.zsh.inc" ]]; then
-   source '/Users/jeff/google-cloud-sdk/completion.zsh.inc'
+if [[ -s '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc' ]]; then
+  source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
 fi
-
