d9c89f92a860dd82d1a3aa627e36ee76a22e78d8
Jeff Billimek <jeffrey_k_billimek@homedepot.com>
2018-04-23 18:39:58 -0400

    extend powerlevel9k for cf prompt

--- zsh/cf.zsh
+++ zsh/cf.zsh
@@ -17,3 +17,27 @@ function cf-copy-env () {
   )"
   # export VCAP_APPLICATION=$(jq '.environment_json' <<< ${JSON_ENV});
 }
+
+
+function cf-get-info () {
+  # get cloudfoundry info
+  if [[ -f ~/.cf/config.json ]]; then
+    cf_foundation=`grep ".Target" ~/.cf/config.json | sed -E 's/.*api\.run[.-]?(.*)\.homedepot\.com.*/\1/'`
+    if [[ -n "$cf_foundation" ]]; then
+      # if we have the jq bin, grab the org and space info, if we're logged in to cf
+      local jq_bin=`command -v jq 2> /dev/null`
+      if [[ -n "$jq_bin" ]]; then
+        local cf_org=`${jq_bin} -r .OrganizationFields.Name ~/.cf/config.json 2>/dev/null`
+        local cf_space=`${jq_bin} -r .SpaceFields.Name ~/.cf/config.json 2>/dev/null`
+      fi
+      if [[ -n "$cf_org" && -n "$cf_space" ]]; then
+        local cf_foundation_string="${cf_foundation}/${cf_org}/${cf_space}"
+      else
+        local cf_foundation_string="${cf_foundation}"
+      fi
+      [[ $cf_foundation == at ]] && echo -n "☁️  ${cf_foundation_string}" # sandbox
+      [[ $cf_foundation =~ np ]] && echo -n "☁️  ${cf_foundation_string}" # non-prod
+      [[ $cf_foundation =~ .*z[a-z].* ]] && echo -n "☁️  ⚠️  ${cf_foundation_string} ⚠️" # production
+    fi
+  fi
+}
