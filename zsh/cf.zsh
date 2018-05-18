04fed88507c11821efe49562b2b8aeb27e6dc04b
Jeff Billimek <jeffrey_k_billimek@homedepot.com>
2018-05-18 09:42:27 -0400

    fixing foundation scanning for cf-get-info

--- zsh/cf.zsh
+++ zsh/cf.zsh
@@ -3,32 +3,28 @@ function cf-copy-env () {
   [[ -z "$1" ]] && return 1;
   APP_GUID=$(cf app "${1}" --guid) || return 1;
   JSON_ENV="$(cf curl /v2/apps/${APP_GUID}/env)"
-  # export VCAP_SERVICES=$(cf curl /v2/apps/${APP_GUID}/env | jq '.system_env_json.VCAP_SERVICES');
-  export VCAP_SERVICES="$(jq '.system_env_json.VCAP_SERVICES' <<< ${JSON_ENV});"
-  # export VCAP_APPLICATION=$(cf curl /v2/apps/${APP_GUID}/env | jq '.application_env_json.VCAP_APPLICATION');
-  export VCAP_APPLICATION="$(jq '.application_env_json.VCAP_APPLICATION' <<< ${JSON_ENV});"
-  # eval $(jq -r '.environment_json as $env | $env | keys | map("export \(.)=" + ($env[.] | @json | @sh)) | .[]' <<< ${JSON_ENV})
+  export VCAP_SERVICES="$(jq '.system_env_json.VCAP_SERVICES' <<<" ${JSON_ENV}");"
+  export VCAP_APPLICATION="$(jq '.application_env_json.VCAP_APPLICATION' <<< "${JSON_ENV}");"
   eval "$(
     jq -r '.environment_json as $env
       | $env
       | keys
       | map("export \(.)=" + ($env[.] | @json))
-      | .[]' <<< ${JSON_ENV}
+      | .[]' <<< "${JSON_ENV}"
   )"
-  # export VCAP_APPLICATION=$(jq '.environment_json' <<< ${JSON_ENV});
 }
 
 
 function cf-get-info () {
   # get cloudfoundry info
   if [[ -f ~/.cf/config.json ]]; then
-    cf_foundation=`grep ".Target" ~/.cf/config.json | sed -E 's/.*api\.run[.-]?(.*)\.homedepot\.com.*/\1/'`
+    cf_foundation=$(grep ".Target" ~/.cf/config.json | sed -E 's/.*api.*run[.-]?(.*)\.homedepot\.com.*/\1/')
     if [[ -n "$cf_foundation" ]]; then
       # if we have the jq bin, grab the org and space info, if we're logged in to cf
-      local jq_bin=`command -v jq 2> /dev/null`
+      local jq_bin=$(command -v jq 2> /dev/null)
       if [[ -n "$jq_bin" ]]; then
-        local cf_org=`${jq_bin} -r .OrganizationFields.Name ~/.cf/config.json 2>/dev/null`
-        local cf_space=`${jq_bin} -r .SpaceFields.Name ~/.cf/config.json 2>/dev/null`
+        local cf_org=$("${jq_bin}" -r .OrganizationFields.Name ~/.cf/config.json 2>/dev/null)
+        local cf_space=$("${jq_bin}" -r .SpaceFields.Name ~/.cf/config.json 2>/dev/null)
       fi
       if [[ -n "$cf_org" && -n "$cf_space" ]]; then
         local cf_foundation_string="${cf_foundation}/${cf_org}/${cf_space}"
