085d6fc17a4eec1db37e67662a4a6394e74632fd
Jeff Billimek <jeff@billimek.com>
2018-06-18 14:58:43 -0400

    adding some kubernetes common aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -218,3 +218,9 @@ alias brewu='brew update  && brew upgrade && brew cleanup && brew prune && brew 
 
 # Docker
 alias dps='docker ps --format "table {{.Names}}\t{{.Status}}"'
+
+# Kubernetes
+alias k='kubectl'
+alias get_k8s_token="kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | cut -f1 -d ' ') | grep -E '^token' | cut -f2 -d':' | tr -d '\t'"
+
+
